cb55429e34c61d42b99852aa2b45a3ab7cf4bf76
Mongisa <ale.monte8888@gmail.com>
2022-11-08 00:56:08 +0100

    create deleteMessages command

---
+++ commands/moderation/deleteMessages.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('delete')
+        .setDescription('Cancella i messaggi nella chat dove viene utilizzato il comando')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+
+        .addNumberOption(option =>
+            option
+                .setName('numero')
+                .setDescription('Numero di messaggi da eliminare')
+                .setMinValue(1)
+                .setMaxValue(100)
+                .setRequired(true)
+        ),
+
+    /**
+    * @param {import('discord.js').Interaction} interaction 
+    */
+    async execute(interaction) {
+        const chatId = interaction.channel.id
+        const number = interaction.options.data[0].value
+
+        interaction.guild.channels.cache.get(chatId).bulkDelete(number)
+
+        interaction.reply({ content: `Messaggi Eliminati \`\`${number}\`\``, ephemeral: true })
+    }
+}
